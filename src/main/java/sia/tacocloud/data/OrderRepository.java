45792f03e6c32a4d8ea46a7e5a94e52c2d0680a0
chao.chen <chenchao@dbappsecurity.com>
2021-09-23 00:37:08 +0800

    use java spring data jdbc

--- src/main/java/sia/tacocloud/data/OrderRepository.java
+++ src/main/java/sia/tacocloud/data/OrderRepository.java
@@ -1,11 +1,8 @@
 package sia.tacocloud.data;
 
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.tacos.TacoOrder;
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
-    
-    TacoOrder save(TacoOrder tacoOrder);
     
 }
--- src/main/java/sia/tacocloud/tacos/TacoOrder.java
+++ src/main/java/sia/tacocloud/tacos/TacoOrder.java
@@ -5,15 +5,20 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
 import java.util.Date;
 
 @Data
+@Table
 public class TacoOrder implements Serializable {
+
     private static final long serialVersionUID = 1L;
-  
+    
+    @Id
     private Long id;
     
     private Date placedAt;
@@ -36,7 +41,6 @@ public class TacoOrder implements Serializable {
     private String ccExpiration;
     @Digits(integer = 3, fraction=0, message="Invalid CVV")
     private String ccCVV;
-    
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco){
